2f59d03ee172c58a714d5ab5028de071c057fa79
Naeem UL-Haq <naeemu@MacBook-Pro.local>
2021-01-17 19:14:16 -0500

    major refactoring to the pipeline based on Doug's changes

--- setupScripts/install_gluu.sh
+++ setupScripts/install_gluu.sh
@@ -1,102 +1,38 @@
 #!/bin/bash
-SIC-AP-TARBAL-VER="SIC-AP-0.0.222"
-SIC-ADMINTOOLS="SIC-Admintools-0.0.26.tgz"
-
-# isntall gluu server 
-echo "setting up repos for gluu"
-wget https://repo.gluu.org/rhel/Gluu-rhel7.repo -O /etc/yum.repos.d/Gluu.repo
-wget https://repo.gluu.org/rhel/RPM-GPG-KEY-GLUU -O /etc/pki/rpm-gpg/RPM-GPG-KEY-GLUU
-rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-GLUU
-yum clean all
-
-# update hosts file with hostname and IP addresses
-echo "updating hosts file with hostname and IP addresses"
-
-zayn=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/name?api-version=2017-08-01&format=text")
-hostname="${zayn}.id.alpha.canada.ca
-ip=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/publicIpAddress?api-version=2017-08-01&format=text")
-privateIP=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/privateIpAddress?api-version=2017-08-01&format=text")
-sed -i.bkp "$ a $ip $hostname" /etc/hosts
-sed -i.bkp "$ a $privateIP $hostname" /etc/hosts
-echo > /etc/hostname
-echo $hostname > /etc/hostname
-
-echo "install azure cli"
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-
-sh -c 'echo -e "[azure-cli]
-name=Azure CLI
-baseurl=https://packages.microsoft.com/yumrepos/azure-cli
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
-
-yum install -y azure-cli
-echo "installing JQ"
-yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum install -y jq
-
-echo "gluu server install begins"
-mkdir staging && cd staging
-wget https://repo.gluu.org/centos/7/gluu-server-4.1.0-centos7.x86_64.rpm
-rpm -Uvh gluu-server-4.1.0-centos7.x86_64.rpm
-#echo "updating the timeouts"
-#sed -i "s/# jetty.server.stopTimeout=5000/jetty.server.stopTimeout=15000/g" /opt/gluu-server/opt/gluu/jetty/identity/start.ini
-#sed -i "s/# jetty.http.connectTimeout=15000/jetty.http.connectTimeout=15000/g" /opt/gluu-server/opt/gluu/jetty/identity/start.ini
-
-echo "enabling gluu server and logging into container"
-/sbin/gluu-serverd enable
-/sbin/gluu-serverd start
-
-API_VER='7.0'
-# Obtain an access token
-TOKEN=$(curl -s 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -H Metadata:true | jq -r '.access_token')
-
-RGNAME=$(curl -s 'http://169.254.169.254/metadata/instance/compute/resourceGroupName?api-version=2020-06-01&format=text' -H Metadata:true)
-KEYVAULT="https://kv-sic-dev-00.vault.azure.net"
-
-SASTOKEN=$(curl -s -H "Authorization: Bearer ${TOKEN}" ${KEYVAULT}/secrets/StorageSaSToken?api-version=${API_VER} | jq -r '.value')
-
-wget -O setup.properties "https://siccommonstorage.blob.core.windows.net/sic-pipeline-artifacts-private/setup.properties?${SASTOKEN}"
-
-echo "update hostname of the gluu server"
-sed -i "/^hostname=/ s/.*/hostname=$hostname/g" setup.properties
-
-echo "copying setup.props file to gluu container"
-cp setup.properties /opt/gluu-server/install/community-edition-setup/
-
-ssh  -o IdentityFile=/etc/gluu/keys/gluu-console -o Port=60022 -o LogLevel=QUIET \
-                -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
-                -o PubkeyAuthentication=yes root@localhost \
-            "/install/community-edition-setup/setup.py -n -f setup.properties"
-
-if [ ! -f /opt/gluu-server/install/community-edition-setup/setup.py ] ; then
-   echo "Gluu setup install failed. Aborting!"
-   exit
+if [ "$#" -ne 1 ]; then
+    echo "Please specify Couchbase IP"
+    echo "./install.sh 10.0.1.4"
+    exit
 fi
 
-#curl -s -H "Authorization: Bearer ${TOKEN}" -F file=@"httpd" https://${RGNAME}-keyvault.vault.azure.net/certificates/httpd/import?api-version=7.1
+TARBALL="SIC-AP-0.0.223"
+KEYVAULT="https://kv-sic-dev-00.vault.azure.net"
 
-sed -i "/^loadData=True/ s/.*/loadData=False/g" setup.properties
+# set the hostname
+echo "Setting the hostname"
+zayn=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/name?api-version=2017-08-01&format=text")
+hostname ${zayn}.id.alpha.canada.ca
 
-echo "downloading SIC tarball"
-#wget https://siccommonstorage.blob.core.windows.net/sic-pipeline-artifacts-public/SIC-Admintools-0.0.26.tgz
-#wget https://siccommonstorage.blob.core.windows.net/sic-pipeline-artifacts-public/SIC-AP-0.0.205.tgz
-wget https://sicqa.blob.core.windows.net/staging/${SIC-AP-TARBAL-VER}.tgz
-wget https://sicqa.blob.core.windows.net/staging/${SIC-ADMINTOOLS}
+# Install epel and jq
+yum clean all
+yum install -y epel-release
+yum install -y jq
 
-tar -xvf $SIC-ADMINTOOLS
+# isntall gluu GPG key TODO: Use alternate supply path
+echo "Setting up Gluuu GPG key"
+wget https://repo.gluu.org/rhel/RPM-GPG-KEY-GLUU -O /etc/pki/rpm-gpg/RPM-GPG-KEY-GLUU
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-GLUU
 
-cp software/install.sh .
-chmod +x install.sh
-cat > install.params <<EOF
-STAGING_URL=https://sicqa.blob.core.windows.net/staging
-KEYVAULT_URL=${KEYVAULT}
-METADATA_URL=https://sicqa.blob.core.windows.net/saml/SIC-Nonprod-signed.xml
-EOF
 
-echo "copying the output folder of the initial install for jks and configs"
-cp -r /opt/gluu-server/install/community-edition-setup/output/ .
+# TODO: Have the Custom Script Extension do this
+echo "Downloading Software"
+wget https://repo.gluu.org/centos/7/gluu-server-4.2.2-centos7.x86_64.rpm
+wget https://raw.githubusercontent.com/sign-in-canada/Admin-Tools/develop/software/install.sh
 
-echo "starting AdminTools setup of SIC tarbal"
-sh install.sh $SIC-AP-TARBAL-VER
+echo "Running SIC setup"
+export STAGING_URL=https://sicqa.blob.core.windows.net/staging
+export KEYVAULT_URL=${KEYVAULT}
+export METADATA_URL=https://sicqa.blob.core.windows.net/saml/SIC-Nonprod-signed.xml
+export CB_HOSTS=${1}
+
+sh ./install.sh ${TARBALL}
